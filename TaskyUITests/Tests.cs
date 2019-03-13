eaba7edc0f8cf13157dfb961a28caf4894ea8bda
ShannenDowling <shannen.dowling@students.ittralee.ie>
2019-03-13 15:55:52 +0000

    Added Smoke Test and Code reference comment

--- TaskyUITests/Tests.cs
+++ TaskyUITests/Tests.cs
@@ -5,6 +5,16 @@ using NUnit.Framework;
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
 
+//Code Reference
+/* Automation, E., 2017. Part 4 - Creating and understanding Xamarin.UITest mobile automation project. [Online] 
+  Available at: https://www.youtube.com/watch?v=iUAjDdgXrQ4&list=PL6tu16kXT9PrMxnBCfaw-24nVNoY-SZ9U&index=4
+ [Accessed 20 02 2019].
+
+ Automation, E., 2017. Part 5 - Record and playback in Xamarin with Xamarin Test Recorder. [Online] 
+ Available at: https://www.youtube.com/watch?v=i7YQysK1dlo&list=PL6tu16kXT9PrMxnBCfaw-24nVNoY-SZ9U&index=5
+ [Accessed 20 02 2019].
+*/
+
 namespace TaskyUITests
 {
     [TestFixture(Platform.Android)]
@@ -29,6 +39,12 @@ namespace TaskyUITests
         public void AppLaunches()
         {
             app.Screenshot("First screen.");
+        }
+
+        [Test]
+        public void SmokeTest()
+        {
+            Assert.That(true);
         }
     }
 }

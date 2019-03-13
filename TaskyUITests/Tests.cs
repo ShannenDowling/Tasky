b67224c3e3fc0d97b4764d5b537f6376a79cc4fc
ShannenDowling <shannen.dowling@students.ittralee.ie>
2019-03-13 19:02:39 +0000

    Revert changes

--- TaskyUITests/Tests.cs
+++ TaskyUITests/Tests.cs
@@ -41,38 +41,7 @@ namespace TaskyUITests
             app.Screenshot("First screen.");
         }
 
-        //// https://nftb.saturdaymp.com/today-i-learned-how-to-create-xamarin-ios-and-android-unit-tests/
-        //[Test]
-        //public void SmokeTest()
-        //{
-        //    Assert.That(true);
-        //}
-
-        //[Test]
-        //public void EnterTextExample()
-        //{
-        //    app.Tap(x => x.Marked("Add Task"));
-        //    app.Tap(x => x.Text("Item name"));
-
-        //    app.Screenshot("Before calling EnterText");
-        //    app.EnterText("The test worked!");
-        //    app.Screenshot("Text entered");
-        //    app.Back();
-        //}
-
-        //// https://github.com/King-of-Spades/AppCenter-Test-Samples/blob/master/Xamarin.UITest/UITestDemo/UITestDemo.UITest
-        //[Test]
-        //public void ClearTextExample()
-        //{
-        //    app.Tap(x => x.Marked("Add Task"));
-        //    app.Tap(x => x.Text("Item name"));
-
-        //    app.Screenshot("Before calling ClearText");
-        //    app.ClearText();
-        //    app.EnterText("The test worked!");
-        //    app.Screenshot("Text cleared & replaced");
-        //    app.Back();
-        //}
+       
     }
 }
 
